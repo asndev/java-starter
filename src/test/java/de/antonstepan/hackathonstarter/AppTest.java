e7c8be4c038ffdb5b225f36baffee483ef4b6bc9
Anton Stepan <info@antonstepan.it>
2016-02-09 10:58:36 +0100

    fixing foobar.

--- src/test/java/de/antonstepan/hackathonstarter/AppTest.java
+++ src/test/java/de/antonstepan/hackathonstarter/AppTest.java
@@ -2,31 +2,14 @@ package de.antonstepan.hackathonstarter;
 
 import static org.assertj.core.api.Assertions.*;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-  /**
-   * Create the test case
-   *
-   * @param testName
-   *          name of the test case
-   */
-  public AppTest(String testName) {
-    super(testName);
-  }
-
-  /**
-   * @return the suite of tests being tested
-   */
-  public static Test suite() {
-    return new TestSuite(AppTest.class);
-  }
-
+public class AppTest {
+ 
+  @Test
   public void testApp() {
     assertThat("foobar").startsWith("foo")
         .endsWith("bar")
